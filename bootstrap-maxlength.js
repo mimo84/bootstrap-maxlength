03a3e467ac87a6712288146196efde268d12d804
Maurizio Napoleoni <napy84@gmail.com>
2013-04-12 21:02:56 +1000

    Version updated to 1.3.1

--- bootstrap-maxlength.js
+++ bootstrap-maxlength.js
@@ -1,5 +1,5 @@
 /* ==========================================================
- * bootstrap-maxlength.js v1.3.0
+ * bootstrap-maxlength.js v1.3.1
  * ==========================================================
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
