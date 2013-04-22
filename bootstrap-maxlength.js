57a3b2b12408c50f014cf6a33eb6b9ee66a70f41
Maurizio Napoleoni <napy84@gmail.com>
2013-04-22 20:08:20 +1000

    Bump up version

--- bootstrap-maxlength.js
+++ bootstrap-maxlength.js
@@ -1,5 +1,5 @@
 /* ==========================================================
- * bootstrap-maxlength.js v1.3.3
+ * bootstrap-maxlength.js v1.3.4
  * ==========================================================
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
