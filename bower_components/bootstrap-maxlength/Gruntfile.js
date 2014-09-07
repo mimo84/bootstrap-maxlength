1e17acea1330f3b9b102df6eef61b164c3f5d896
Maurizio Napoleoni <napy84@gmail.com>
2014-09-07 13:33:12 +1000

    Initial clean up and update

--- bower_components/bootstrap-maxlength/Gruntfile.js
+++ bower_components/bootstrap-maxlength/Gruntfile.js
@@ -1,6 +1,11 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  // Automatically load grunt modules
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -16,13 +21,13 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         banner: '/* ========================================================== \n' +
-                ' * \n' +
-                ' * <%= pkg.name %>.js v <%= pkg.version %> \n' +
-                ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-                ' * Licensed under <%= pkg.licenses[0]["type"] %>\n' +
-                ' * URL: <%= pkg.licenses[0]["url"] %>\n' +
-                ' *\n' +
-                ' * ========================================================== */\n\n',
+          ' * \n' +
+          ' * <%= pkg.name %>.js v <%= pkg.version %> \n' +
+          ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+          ' * Licensed under <%= pkg.licenses[0]["type"] %>\n' +
+          ' * URL: <%= pkg.licenses[0]["url"] %>\n' +
+          ' *\n' +
+          ' * ========================================================== */\n\n'
       },
       dist: {
         files: {
@@ -39,20 +44,21 @@ module.exports = function(grunt) {
         jshintrc: '.jshintrc'
       }
     },
+    jscs: {
+      options: {
+        config: '.jscsrc'
+      },
+      src: {
+        src: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+      }
+    },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint', 'qunit']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('test', ['jshint','jscs','qunit']);
 
-  grunt.registerTask('test', ['jshint','qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-
+  grunt.registerTask('default', ['jshint','qunit','concat','uglify']);
 };
--- bower_components/bootstrap-maxlength/test/tests.js
+++ bower_components/bootstrap-maxlength/test/tests.js
@@ -1,90 +1,79 @@
 $(function () {
-    'use strict';
-    module('maxlength');
+  'use strict';
 
-    test('Maxlength is displayed correctly', function () {
-      $('<input type="text" maxlength="10" />')
-          .appendTo('#qunit-fixture')
-          .maxlength().focus();
+  var maxlengthInput;
 
-      ok($('.bootstrap-maxlength').length, 'maxlength was inserted');
-
-      $('.bootstrap-maxlength').remove();
-
-      $('#qunit-fixture').empty();
-    });
-
-    test('Maxlength is visible on focus', function () {
-      $('<input type="text" maxlength="10" />')
-          .appendTo('#qunit-fixture')
-          .maxlength().end().focus();
-
-      ok($('.bootstrap-maxlength').is(':visible'), 'Maxlength is visible');
-
-      $('.bootstrap-maxlength').remove();
-
-      $('#qunit-fixture').empty();
-    });
-
-    test('Maxlength is invisible on blur', function () {
-      var maxlengthInput = $('<input type="text" maxlength="10" />')
-          .appendTo('#qunit-fixture')
-          .maxlength();
-
-          maxlengthInput.focus();
-          maxlengthInput.blur();
-          ok( !$('.bootstrap-maxlength').length, 'Maxlength is not visible');
-          $('.bootstrap-maxlength').remove();
-          $('#qunit-fixture').empty();
-    });
-
-    test('Maxlength updates the maxlength', function () {
-
-      var maxlengthInput = $('<input type="text" maxlength="10" />')
-          .appendTo('#qunit-fixture');
+  module('maxlength', {
+    setup: function () {
+      maxlengthInput = $('<input type="text" maxlength="10" />')
+        .appendTo('#qunit-fixture');
 
       maxlengthInput.maxlength();
-      maxlengthInput.focus();
-
-      // Change the maxlength attribute
-      maxlengthInput.blur();
-      maxlengthInput.attr('maxlength','142');
-
-      maxlengthInput.focus();
-
-      ok($('.bootstrap-maxlength').html() === '0 / 142' , 'Maxlength updated the field');
+    },
+    teardown: function () {
       $('.bootstrap-maxlength').remove();
       $('#qunit-fixture').empty();
+    }
+  });
 
-    });
+  test('Maxlength is displayed correctly', function () {
+    maxlengthInput.focus();
+    ok($('.bootstrap-maxlength').length, 'maxlength was inserted');
+  });
 
-    test('The focus event is triggered multiple times without a blur', function () {
+  test('Maxlength is visible on focus', function () {
+    maxlengthInput.focus();
+    ok($('.bootstrap-maxlength').is(':visible'), 'Maxlength is visible');
+  });
 
-      var maxlengthInput = $('<input type="text" maxlength="10" />')
-          .appendTo('#qunit-fixture');
+  test('Maxlength is removed on blur', function () {
+    maxlengthInput.maxlength().focus().blur();
+    ok(!$('.bootstrap-maxlength').length, 'Maxlength is removed on blur');
+  });
+
+  test('Maxlength updates the maxlength', function () {
+    maxlengthInput.focus();
+
+    // Change the maxlength attribute
+    maxlengthInput.blur().attr('maxlength', '142').focus();
+
+    ok($('.bootstrap-maxlength').html() === '0 / 142', 'Maxlength updated the field');
+
+  });
+
+  test('Removing an element with the maxlength removes the maxlength if any.', function () {
+    maxlengthInput.maxlength().focus();
+    maxlengthInput.remove();
+    ok($('.bootstrap-maxlength').length === 0, 'Maxlength field removed with the input');
+
+  });
+
+  test('The focus event is triggered multiple times without a blur', function () {
+    maxlengthInput.focus().focus().focus().focus();
+    ok($('.bootstrap-maxlength').length === 1, 'Maxlength visualized only once after multiple focuses');
+  });
+
+  module('textarea', {
+    setup: function () {
+      maxlengthInput = $('<textarea maxlength="10"></textarea>')
+        .appendTo('#qunit-fixture');
 
       maxlengthInput.maxlength();
-
-      maxlengthInput.focus();
-      maxlengthInput.focus();
-      maxlengthInput.focus();
-      maxlengthInput.focus();
-
-      ok($('.bootstrap-maxlength').length === 1, 'Maxlength visualized only once after multiple focuses');
+    },
+    teardown: function () {
       $('.bootstrap-maxlength').remove();
       $('#qunit-fixture').empty();
+    }
+  });
 
-    });
+  test('Newlines are not counted twice', function () {
+    maxlengthInput.val('t\r\nt');
 
-    test('Newlines are not counted twice', function() {
-      var maxlengthInput = $('<textarea maxlength="10"></textarea>').val('t\r\nt')
-          .appendTo('#qunit-fixture');
+    maxlengthInput.maxlength();
+    maxlengthInput.focus();
 
-      maxlengthInput.maxlength();
-      maxlengthInput.focus();
+    ok($('.bootstrap-maxlength').html() === '3 / 10', 'Current length is: ' + $('.bootstrap-maxlength').html() + '. Expected 3 / 10.');
 
-      ok($('.bootstrap-maxlength').html() === '3 / 10' , 'Current length is: '+$('.bootstrap-maxlength').html()+'. Expected 3 / 10.');
-
-    });
+  });
 
 });
