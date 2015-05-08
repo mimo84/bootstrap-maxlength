3b192faa442bfc2cbf350134aa00b953f46d7556
Matt Krueger <mkruege2@gmail.com>
2015-05-08 12:06:26 -0500

    Updated tests for placement option

--- test/tests.js
+++ test/tests.js
@@ -140,4 +140,80 @@ $(function () {
     ok(maxlengthInput.attr('data-bs-mxl') === '10', 'data-bs-mxl attribute value is ' + maxlengthInput.attr('data-bs-mxl') + '. Expected value of 10.');
   });
 
+
+  module('placement object option', {
+    setup: function () {
+      maxlengthInput = $('<input type="text" maxlength="10" />')
+          .appendTo('#qunit-fixture');
+
+      maxlengthInput.maxlength({
+        placement : {
+          top: '5px',
+          left: '6px',
+          bottom: '7px',
+          right: '10px'
+        }
+      });
+    },
+    teardown: function () {
+      $('.bootstrap-maxlength').remove();
+      $('#qunit-fixture').empty();
+    }
+  });
+
+  test('css top placement from object placement option', function () {
+    maxlengthInput.focus();
+    var hasTop = $('.bootstrap-maxlength').attr('style').match(/top\:\s?5px/).length === 1;
+    ok(hasTop, 'maxlength has expected top style');
+  });
+
+  test('css left placement from object placement option', function () {
+    maxlengthInput.focus();
+    var hasLeft = $('.bootstrap-maxlength').attr('style').match(/left\:\s?6px/).length === 1;
+    ok(hasLeft, 'maxlength has expected left style');
+  });
+
+  test('css right placement from object placement option', function () {
+    maxlengthInput.focus();
+    var hasRight = $('.bootstrap-maxlength').attr('style').match(/right\:\s?10px/).length === 1;
+    ok(hasRight, 'maxlength has expected right style');
+  });
+
+  test('css bottom placement from object placement option', function () {
+    maxlengthInput.focus();
+    var hasBottom = $('.bootstrap-maxlength').attr('style').match(/bottom\:\s?7px/).length === 1;
+    ok(hasBottom, 'maxlength has expected bottom style');
+  });
+
+  var wasCalled,
+      argsLength;
+
+  module('placement function option', {
+    setup: function () {
+      wasCalled = false;
+      maxlengthInput = $('<input type="text" maxlength="10" />')
+          .appendTo('#qunit-fixture');
+
+      maxlengthInput.maxlength({
+        placement : function () {
+          wasCalled = true;
+          argsLength = arguments.length;
+        }
+      });
+    },
+    teardown: function () {
+      $('.bootstrap-maxlength').remove();
+      $('#qunit-fixture').empty();
+    }
+  });
+
+  test('Was called', function () {
+    maxlengthInput.focus();
+    ok(wasCalled, 'Custom placement function was called');
+  });
+  test('Was called with expected number of arguments', function () {
+    maxlengthInput.focus();
+    ok(argsLength === 3, 'placement function option was called with expected number of arguments');
+  });
+
 });
